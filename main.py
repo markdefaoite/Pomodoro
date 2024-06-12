789c8050e99d0c9eed963b4638782de5bb62a35c
Mark White <markdefaoite@hotmail.com>
2024-06-12 14:17:13 -0400

    finishing the UI layout

--- main.py
+++ main.py
@@ -27,9 +27,24 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer")
+timer_label.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+timer_label.grid(column=1, row=0)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
 
 
+check ="✔"
+
+check_label = Label(text=check)
+check_label.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 14, "bold"))
+check_label.grid(column=1, row=3)
 window.mainloop()
 
 
