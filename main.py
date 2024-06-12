a8e6edb8a34b2c3ce540f0fb28c25c3c32c2b325
markdefaoite <markdefaoite@hotmail.com>
2024-06-12 14:51:04 -0400

    adding timer with correct formatting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,13 +11,27 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # https://colorhunt.co/
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_minutes = math.floor(count / 60)
+    count_seconds = count % 60
+    if count_seconds < 10:
+        count_seconds = f"0{count_seconds}"
+
+    canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -26,25 +42,22 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 30, "bold"))
 canvas.grid(column=1, row=1)
 
 timer_label = Label(text="Timer")
 timer_label.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
 timer_label.grid(column=1, row=0)
 
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset")
 reset_button.grid(column=2, row=2)
 
-
-check ="✔"
+check = "✔"
 
 check_label = Label(text=check)
 check_label.config(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 14, "bold"))
 check_label.grid(column=1, row=3)
 window.mainloop()
-
-
